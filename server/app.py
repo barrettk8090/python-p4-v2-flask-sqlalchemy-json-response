bc3617f251edbee4fdab3399a375ae67761f2a14
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-08 13:07:43 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -16,16 +16,18 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species
+                }
+    return make_response(pet_dict, 200)
 
 
 if __name__ == '__main__':
